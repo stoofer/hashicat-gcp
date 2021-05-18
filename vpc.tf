3533874a84b138fd28b03c0c5221feae55dfc4df
Stuart Caborn <stooferus@gmail.com>
2021-05-18 10:19:24 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+network_name = stuart-network
+
+project_id = var.project
+
+subnets = [
+  {
+    subnet_name   = "stuart-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
