fe71c134a256acbfa2199c4753da6365c8d2c268
Stuart Caborn <stooferus@gmail.com>
2021-05-18 10:34:20 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/stuartcaborn-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = "stuart-network"
+  network_name = "stuart-network2"
 
     project_id = var.project
 
