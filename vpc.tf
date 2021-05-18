9d1ecb80e4c7cc0f96ef2bf7f9f66ab914383b89
Stuart Caborn <stooferus@gmail.com>
2021-05-18 10:24:36 +0000

    Added vpc module 3

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,16 @@
-network_name = "stuart-network"
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
+    
+    network_name = "stuart-network"
 
-project_id = var.project
+    project_id = var.project
 
-subnets = [
-  {
-    subnet_name   = "stuart-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    subnets = [
+            {
+                subnet_name   = "stuart-subnet"
+                subnet_ip     = "10.100.10.0/24"
+                subnet_region = var.region
+            }
+        ]
+}
